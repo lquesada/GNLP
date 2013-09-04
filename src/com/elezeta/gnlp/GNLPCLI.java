aba1531b1ad3609498d122297def89ec46ae39f6
Luis Quesada <luis@elezeta.com>
2013-09-04 15:02:24 +0200

    Added build.xml and junit lib files

--- src/com/elezeta/gnlp/GNLPCLI.java
+++ src/com/elezeta/gnlp/GNLPCLI.java
@@ -3,7 +3,30 @@ package com.elezeta.gnlp;
 public class GNLPCLI {
 
 	public static void main(String[] args) {
-		
+		if (args.length == 0) {
+			showHelp();
+		}
 	}
 	
+	public static void showHelp() {
+		System.out.println("GNLP (General Natural Language Parser) - Command-Line Interface");
+		System.out.println("Copyright (c) 2013, Luis Quesada - https://github.com/lquesada");
+	    System.out.println("");
+	    System.out.println("Usage: java -jar GNLP.jar <language> <sentence>");
+	    System.out.println("");
+	    System.out.println("Example: java -jar GNLP.jar en \"I saw a picture of New York\"");
+	    System.out.println("");
+	    System.out.println("Supported languages:");
+	    System.out.println("");
+	    System.out.println("- dummy");
+	    System.out.println("");
+	    System.out.println("  Specify language elements directly: ");
+	    System.out.println("    CoordinatingConjunction, SubordinatingConjunction, Verb, Adverb, Preposition,");
+	    System.out.println("    Determiner, CommonNoun, ProperNoun, Pronoun");
+	    System.out.println("");
+	    System.out.println("  Example sentence: ");
+	    System.out.println("");
+	    System.out.println("  I(CommonNoun/ProperNoun) saw(CommonNoun/Verb) a(Determiner) tree(CommonNoun)");
+	}
 }
+	    
